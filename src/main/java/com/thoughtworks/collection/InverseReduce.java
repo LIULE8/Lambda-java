0edb6252994c201e8e7c2a70a2657782276df46e
LIULE8 <scau_lzc@163.com>
2018-07-10 21:55:57 +0800

    finish AddTest should_be_divided_to_smaller_number()

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,20 +2,26 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> collect = IntStream.range(0, number - 1).boxed().collect(Collectors.toList());
+        Collections.reverse(collect);
+        return collect.stream().filter(x -> (number - x) % 2 == 0).collect(Collectors.toList());
     }
 }
