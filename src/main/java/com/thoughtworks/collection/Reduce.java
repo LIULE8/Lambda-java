9557a08e88331fdf963cc93523b81708cb612101
LIULE8 <scau_lzc@163.com>
2018-07-10 22:20:16 +0800

    finish should_return_first_even_element()

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -31,7 +31,7 @@ public class Reduce {
     }
 
     public int getFirstEven() {
-        throw new NotImplementedException();
+        return arrayList.stream().filter(x -> x % 2 == 0).collect(Collectors.toList()).get(0);
     }
 
     public int getIndexOfFirstEven() {
