bb89a74703f32099d1d6cfb5a87ff7e6f0b98d35
LIULE8 <scau_lzc@163.com>
2018-07-10 21:20:24 +0800

    finish AddTest should_pop_common_elements()

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -1,9 +1,12 @@
 package com.thoughtworks.collection;
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+import org.apache.commons.collections.CollectionUtils;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collector;
@@ -33,10 +36,14 @@ public class CollectionOperator {
     }
 
     public List<Integer> popEvenElments(int[] array) {
+        List<Integer> list = buildListsFromArray(array);
+        return list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+    }
+
+    private List<Integer> buildListsFromArray(int[] array) {
         String str = Arrays.toString(array);
         String[] s = str.substring(1, str.lastIndexOf("]")).split(",");
-        List<String> strings = Arrays.asList(s);
-        return strings.stream().map(x -> Integer.parseInt(x.trim())).filter(x -> x % 2 == 0).collect(Collectors.toList());
+        return Arrays.stream(s).map(x -> Integer.parseInt(x.trim())).collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
@@ -44,7 +51,9 @@ public class CollectionOperator {
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> firstList = buildListsFromArray(firstArray);
+        List<Integer> secondList = buildListsFromArray(secondArray);
+        return firstList.stream().filter(secondList::contains).collect(Collectors.toList());
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
