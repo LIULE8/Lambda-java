09c6221e5864cbf001346a630ae6b39b1c4c8236
LIULE8 <scau_lzc@163.com>
2018-07-15 10:45:33 +0800

    update CollectionOperator

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -1,21 +1,18 @@
 package com.thoughtworks.collection;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
         if (right > left) {
             return IntStream.range(left, right + 1).boxed().collect(Collectors.toList());
         } else {
-            List<Integer> collect = IntStream.range(right, left + 1).boxed().collect(Collectors.toList());
-            Collections.reverse(collect);
-            return collect;
+            return IntStream.range(right, left + 1).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         }
     }
 
@@ -23,21 +20,12 @@ public class CollectionOperator {
         if (right > left) {
             return IntStream.range(left, right + 1).boxed().collect(Collectors.toList()).stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
         } else {
-            List<Integer> collect = IntStream.range(right, left + 1).boxed().collect(Collectors.toList()).stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-            Collections.reverse(collect);
-            return collect;
+            return IntStream.range(right, left + 1).boxed().filter(x -> x % 2 == 0).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         }
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        List<Integer> list = buildListsFromArray(array);
-        return list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-    }
-
-    private List<Integer> buildListsFromArray(int[] array) {
-        String str = Arrays.toString(array);
-        String[] s = str.substring(1, str.lastIndexOf("]")).split(",");
-        return Arrays.stream(s).map(x -> Integer.parseInt(x.trim())).collect(Collectors.toList());
+        return Arrays.stream(array).boxed().filter(x -> x % 2 == 0).collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
@@ -45,18 +33,14 @@ public class CollectionOperator {
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        List<Integer> firstList = buildListsFromArray(firstArray);
-        List<Integer> secondList = buildListsFromArray(secondArray);
-        return firstList.stream().filter(secondList::contains).collect(Collectors.toList());
+        List<Integer> secondList = Arrays.stream(secondArray).boxed().collect(Collectors.toList());
+        return Arrays.stream(firstArray).boxed().filter(secondList::contains).collect(Collectors.toList());
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        List<Integer> list = new ArrayList<>();
-        List<Integer> firstList = Arrays.asList(firstArray);
-        List<Integer> secondList = Arrays.asList(secondArray);
-        List<Integer> collect = secondList.stream().filter(x -> !firstList.contains(x)).collect(Collectors.toList());
-        list.addAll(firstList);
-        list.addAll(collect);
-        return list;
+        List<Integer> firstList = Arrays.stream(firstArray).collect(Collectors.toList());
+        List<Integer> list = Arrays.stream(secondArray).filter(x -> !firstList.contains(x)).collect(Collectors.toList());
+        firstList.addAll(list);
+        return firstList;
     }
 }
